cd204f7a1f4161301ff17dd3f51fbc7e80fc6c91
Mallory Iden <malloryworks@gmail.com>
2024-11-18 19:54:04 -0800

    Updated css

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,36 +1,26 @@
-import { ComponentProps } from 'react';
-import styles from './button.module.scss';
-import classnames from 'classnames';
+import { ComponentProps } from "react";
+import styles from "./button.module.scss";
+import classnames from "classnames";
 
-export type ButtonProps = ComponentProps<'button'> & {
-	type: 'Solid' | 'Outline' | 'Unstyled';
-	variant: 'Primary' | 'Secondary';
-	size: 'Standard' | 'Big';
+export type ButtonProps = ComponentProps<"button"> & {
+  type: "Solid" | "Outline" | "Unstyled";
+  mainColor: "Primary" | "Secondary" | 'Accent';
+  size: "Standard" | "Big";
 };
 
-export const Button = ({ type = 'Solid', variant = 'Primary', size = 'Standard', className, ...props }: ButtonProps) => {
-	let allClasses = classnames(styles.button, styles[type], styles[variant], styles[size], className);
-	return <button {...props} className={allClasses} />
-	
-	// return (
-	// 	<button
-	// 		className={classnames(styles.button, {
-	// 			[styles.Primary]: variant === 'Primary',
-	// 			[styles.Secondary]: variant === 'Secondary',
-	// 			[styles.AccentCool]: variant === 'Accent Cool',
-	// 			[styles.AccentWarm]: variant === 'Accent Warm',
-	// 			[styles.sizeStandard]: size === 'Standard',
-	// 			[styles.sizeBig]: size === 'Big',
-	// 			[styles.Outline]: size === 'Outline',
-	// 		})}
-	// 		{...props}
-	// 	/>
-	// );
+export const Button = ({
+  type = "Solid",
+  mainColor = "Primary",
+  size = "Standard",
+  className,
+  ...props
+}: ButtonProps) => {
+  let allClasses = classnames(
+    styles.button,
+    styles[type],
+    styles[mainColor],
+    styles[size],
+    className,
+  );
+  return <button {...props} className={allClasses} />;
 };
-
-
-// export const Button = ({ variant = 'Default', size = 'Standard', className, ...props }: ButtonProps) => {
-// 	let classes = clsx(styles.button, styles[variant], styles[size], className);
-
-// 	return <button {...props} className=[classes] />
-// };
