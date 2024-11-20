ad7d45e4f4f94492456a5758ddb27edd602df922
Mallory Iden <malloryworks@gmail.com>
2024-11-20 06:49:33 -0800

    Fixed big button styling

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -11,7 +11,7 @@ export type ButtonProps = ComponentProps<"button"> & {
 export const Button = ({
   type = "Solid",
   mainColor = "Primary",
-  size = false,
+  size = 'Standard',
   className,
   ...props
 }: ButtonProps) => {
